e3243341d3e892d053c1e4b769b85345543fbcfd
Younghun Choi <choi0hun@gmail.com>
2013-04-13 10:41:42 +0900

    bug fix

--- lib/generators/nifty/scaffold/templates/controller.rb
+++ lib/generators/nifty/scaffold/templates/controller.rb
@@ -1,7 +1,7 @@
 class <%= plural_class_name %>Controller < ApplicationController
   <%= controller_methods :actions %>
   private
-  def <%= class_name %>_params
+  def <%= singular_name %>_params
     params.require(:<%= class_name %>).permit(<%= model_attributes.map { |a| ":#{a.name}" }.join(", ") %>)
   end
 end
