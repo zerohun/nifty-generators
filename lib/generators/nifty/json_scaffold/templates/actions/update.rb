a4d715622e48b104160e82fd496c1cfd03d9fb6c
Younghun Choi <choi0hun@gmail.com>
2013-04-15 11:58:40 +0900

    refactoring

--- lib/generators/nifty/json_scaffold/templates/actions/update.rb
+++ lib/generators/nifty/json_scaffold/templates/actions/update.rb
@@ -1,13 +1,8 @@
   def update
-    begin
-      @<%= instance_name %> = <%= relation_name %>.find(params[:id])
-      @<%= instance_name %>.update_attributes(<%= class_name.underscore %>_params)
-      @jsonResult.object = <%= instance_name %>
-      @jsonResult.status = true
-    rescue Exception => e
-      @jsonResult.status = false
-      @jsonResult.message = "error"
-      throw e if Rails.env != "production"
-    end
+    @error_message = "error"
+    
+    @<%= instance_name %> = <%= relation_name %>.find(params[:id])
+    @<%= instance_name %>.update_attributes(<%= class_name.underscore %>_params)
+    @jsonResult.object = <%= instance_name %>
 
   end
